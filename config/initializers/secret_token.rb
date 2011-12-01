bd582198f96458ddacbd3494f9ce2c8880726bcf
Catalino Cuadrado <ccuadrado@gmail.com>
2011-12-01 11:57:14 -0500

    First commit for Crawler app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crawler::Application.config.secret_token = 'cb8e19407feb397a7fce99c4c2baed279b940e601be856f2fc4c9e628691b834670b1ff21bc6cfcb6badec9452d6d72604574d1f1bea32a95e2724738d939d70'
