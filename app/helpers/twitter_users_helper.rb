943b2956cb6755d8623bcb2a0da655a254431baf
Catalino <ccuadrado@gmail.com>
2011-12-07 00:33:21 -0500

    Added Twitter user authentication methods.
    Get twitter hastags with Twitter.search('#hashtag') better provide an options hash or list is humongous. Will figure out how to parse it tomorrow, I've kinda got a hold on it but there are some thingswhich are bugging me like the fact that it returns an array first,so you get it with like a = Twitter.search('stuff') and then a[0].id will give you the tweeter's user ID.

--- app/helpers/twitter_users_helper.rb
+++ app/helpers/twitter_users_helper.rb
@@ -1,2 +1,23 @@
+require 'twitter'
+
 module TwitterUsersHelper
+
+  def configure_authentication
+    consumer_secret = "18yElxR1Zf8ESVkl3k7XQZxyAPWngz5iM69nbhH7yE"
+    consumer_key = "zQ727fZBHDIv36pKhr2Hg"
+
+    Twitter.configure do |config|
+      config.consumer_key = consumer_key
+      config.consumer_secret = consumer_secret
+      config.oauth_token = "157879876-iSPfgtHxw8QSAj6cJl0uYTbDTV1kfxsw8Tgi1QGK"
+      config.oauth_token_secret = "XiI1kkuGgvqZNc4mGIGkPxjcr19p9PVxhT7m0M"
+      Twitter::Client.new
+    end
+  end
+
+
+
+
+
+
 end
