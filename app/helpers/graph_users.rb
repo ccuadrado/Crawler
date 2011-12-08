e7e9bcd2808ee3d550f0836f532803a9eea3bda5
Catalino <ccuadrado@gmail.com>
2011-12-08 01:41:33 -0500

    Fixed a bug that created some duplicates
    I checked for duplicates in the set but then was checking by id # and was adding them by name...so they would never conflict...

--- app/helpers/graph_users.rb
+++ app/helpers/graph_users.rb
@@ -9,7 +9,7 @@ module GraphUsers
     users = Set.new
     15.times do |t|
     result = Twitter.search(tag.to_s,{:rpp => 100, :page => t+1, :with_twitter_user_id => 1})
-    result.map { |tweet| users.include?(tweet.from_user) ?  puts("Duplicate Detected") : users.add(tweet.from_user_id) }    
+    result.map { |tweet| users.include?(tweet.from_user) ?  puts("Duplicate Detected") : users.add(tweet.from_user) }    
     end
   users
   end
